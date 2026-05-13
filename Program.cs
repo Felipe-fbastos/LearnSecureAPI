5a42659a2bd022f3dabf5c525e46fbc2ba17e192
Felipe-fbastos <felipeferreirabastos2@gmail.com>
2026-05-12 21:50:35 -0300

    Comentei o if do swagger

--- Program.cs
+++ Program.cs
@@ -47,11 +47,11 @@ builder.Services.AddScoped<TokenService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 // Deve ser nessa ordem (Se inverter vai quebrar)
